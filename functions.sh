4cb44216fcf29aae8e17e778fbf8082f6495fe24
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2009-10-02 16:19:46 +0200

    new osx binary test

--- functions.sh
+++ functions.sh
@@ -11,3 +11,13 @@ function guess_olx_dir() {
 	# We could check several dirs here now. This is just how I have it.
 	echo "${curdir}/../openlierox"
 }
+
+# $1 - macosx .app bundle
+function is_olx_macosx_bin() {
+	test -e "$1/Contents/Info.plist" && \
+	test -d "$1/Contents/MacOS" && \
+	return 0
+	# no macosx binary
+	return 1
+}
+
