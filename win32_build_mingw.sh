d439cd9f69ca3aab31a3d13c87549cdb9b0006e3
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2010-01-28 02:27:57 +0100

    more fixes for mingw

--- win32_build_mingw.sh
+++ win32_build_mingw.sh
@@ -6,27 +6,34 @@ cd "${distribdir}"
 source functions.sh
 
 olxdir="$(guess_olx_dir)"
+distribdir="$(pwd)"
 
 mkdir -p win32build
 cd win32build
 
 mingwdir="${distribdir}/mingw"
 
-export CC="i586-mingw32msvc-cc"
-export CXX="i586-mingw32msvc-c++"
+cprefix="/usr/bin/i586-mingw32"
+export CC="${cprefix}-gcc"
+export CXX="${cprefix}-c++"
 
-export CFLAGS="-I\"${mingwdir}/include\""
+CFLAGS="$CFLAGS -I${mingwdir}/include"
+CFLAGS="$CFLAGS -I${mingwdir}/include/SDL -DMINGW"
+export CFLAGS
 export CXXFLAGS="$CFLAGS"
 
-#	-D CMAKE_EXE_LINKER_FLAGS="-L\\\\\\\"${mingwdir}/lib\\\\\\\" -u _WinMain@16" \
 
 
 mv "${olxdir}/CMakeCache.txt" "${olxdir}/CMakeCache.txt.old" 2>/dev/null
 cachemoved=$?
 
 cmake \
-	-D MINGW_CROSS_COMPILE=1 -D BREAKPAD=0 \
+	-D CMAKE_C_COMPILER="$CC" \
+	-D CMAKE_CXX_COMPILER="$CXX" \
+	-D MINGW_CROSS_COMPILE=1 \
 	-D LINKER_LANGUAGE="CXX" \
+	-D BREAKPAD=0 \
+	-D CMAKE_EXE_LINKER_FLAGS="-L${mingwdir}/lib -u _WinMain@16 -static-libgcc -mwindows" \
 	"${olxdir}"
 cmakerun=$?
 
@@ -34,4 +41,4 @@ cmakerun=$?
 
 [ "$cmakerun" != "0" ] && echo "cmake failed" && exit 1
 
-make -j4 VERBOSE=1
+make -j4
